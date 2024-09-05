3206d795aec9fe6f81748fb5fc5abd0ec4ff32d3
HanLin <ruanbiwen@newland.com.cn>
2024-09-05 20:46:16 +0800

    格式化代码

--- assets/vue.f36acd1f.e54c09cc.js
+++ assets/vue.f36acd1f.e54c09cc.js
@@ -1,36 +1,49 @@
-import { y as injectHead, r as ref, z as watchEffect, A as resolveUnrefHeadInput, B as watch, b as onBeforeUnmount, C as onDeactivated, D as onActivated, E as getCurrentInstance } from "./entry.e4388931.js";
+import {
+    y as injectHead,
+    r as ref,
+    z as watchEffect,
+    A as resolveUnrefHeadInput,
+    B as watch,
+    b as onBeforeUnmount,
+    C as onDeactivated,
+    D as onActivated,
+    E as getCurrentInstance
+} from "./entry.e4388931.js";
+
 function useHead(input, options = {}) {
-  const head = options.head || injectHead();
-  if (head) {
-    if (!head.ssr)
-      return clientUseHead(head, input, options);
-    return head.push(input, options);
-  }
+    const head = options.head || injectHead();
+    if (head) {
+        if (!head.ssr)
+            return clientUseHead(head, input, options);
+        return head.push(input, options);
+    }
 }
+
 function clientUseHead(head, input, options = {}) {
-  const deactivated = ref(false);
-  const resolvedInput = ref({});
-  watchEffect(() => {
-    resolvedInput.value = deactivated.value ? {} : resolveUnrefHeadInput(input);
-  });
-  const entry = head.push(resolvedInput.value, options);
-  watch(resolvedInput, (e) => {
-    entry.patch(e);
-  });
-  const vm = getCurrentInstance();
-  if (vm) {
-    onBeforeUnmount(() => {
-      entry.dispose();
+    const deactivated = ref(false);
+    const resolvedInput = ref({});
+    watchEffect(() => {
+        resolvedInput.value = deactivated.value ? {} : resolveUnrefHeadInput(input);
     });
-    onDeactivated(() => {
-      deactivated.value = true;
+    const entry = head.push(resolvedInput.value, options);
+    watch(resolvedInput, (e) => {
+        entry.patch(e);
     });
-    onActivated(() => {
-      deactivated.value = false;
-    });
-  }
-  return entry;
+    const vm = getCurrentInstance();
+    if (vm) {
+        onBeforeUnmount(() => {
+            entry.dispose();
+        });
+        onDeactivated(() => {
+            deactivated.value = true;
+        });
+        onActivated(() => {
+            deactivated.value = false;
+        });
+    }
+    return entry;
 }
+
 export {
-  useHead as u
+    useHead as u
 };
